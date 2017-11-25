c6ec69af5cb7c27d596673c5e26b0f755100931e
kucinghitam13 <dika.adhitama@gmail.com>
2017-11-25 12:54:27 +0700

    Tampilan Kuis

---
+++ app/src/main/java/com/example/android/iak_beginner/Pertanyaan2Activity.java
@@ -0,0 +1,13 @@
+package com.example.android.iak_beginner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pertanyaan2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pertanyaan2);
+    }
+}
